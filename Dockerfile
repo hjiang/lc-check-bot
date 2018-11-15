85443af6ccb158433b8cd78735b8aaafc06eb93c
Hong Jiang <j@1byte.io>
2018-11-16 01:04:07 +0800

    :fire: Remove unnecessary script

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM node:11-alpine
 ADD hubot /hubot
 WORKDIR /hubot
 RUN npm install
-ENTRYPOINT ["/hubot/start_prod.sh"]
+ENTRYPOINT ["bin/hubot", "-a", "zulip"]
--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
-source .lc_keys
+#!/bin/sh
+
+set -e
+
+source $HOME/.lc_check_botrc
 docker run \
-  -e CN_LC_APP_ID=$CN_LC_APP_ID \
-  -e CN_LC_APP_KEY=$CN_LC_APP_KEY \
-  -e CN_LC_LE_WEB_URL=$CN_LC_LE_WEB_URL \
-  -e US_LC_APP_ID=$US_LC_APP_ID \
-  -e US_LC_APP_KEY=$US_LC_APP_KEY \
-  -e US_LC_LE_WEB_URL=$US_LC_LE_WEB_URL \
+  -e CN_LC_APP_ID \
+  -e CN_LC_APP_KEY \
+  -e CN_LC_LE_WEB_URL \
+  -e US_LC_APP_ID \
+  -e US_LC_APP_KEY \
+  -e US_LC_LE_WEB_URL \
+  -e HUBOT_ZULIP_BOT \
+  -e HUBOT_ZULIP_API_KEY \
+  -e HUBOT_ZULIP_SITE \
+  -e HUBOT_ZULIP_ONLY_SUBSCRIBED_STREAMS \
   -d --name r2 check-bot
