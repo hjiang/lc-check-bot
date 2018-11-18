d9d0bb55ad4e94fc0ed29600d205ce661d664564
Hong Jiang <j@1byte.io>
2018-11-18 14:00:33 +0800

    :sparkles: Add check for RTM

--- hubot/scripts/check-lc-health.js
+++ hubot/scripts/check-lc-health.js
@@ -1,11 +1,17 @@
 const storage = require('leancloud-storage');
+const { Realtime, TextMessage } = require('leancloud-realtime');
 const fetch = require('node-fetch');
 
 var g_region = null;
+var g_rtm = null;
 
 function initSdkForUS() {
   storage.applicationId = null;
   storage.init({
+    appId: process.env.US_LC_APP_ID,
+    appKey: process.env.US_LC_APP_KEY
+  });
+  g_rtm = new Realtime({
     appId: process.env.US_LC_APP_ID,
     appKey: process.env.US_LC_APP_KEY
   });
@@ -16,6 +22,10 @@ function initSdkForCN() {
   storage.applicationId = null;
   storage._config.serverURLs = {};
   storage.init({
+    appId: process.env.CN_LC_APP_ID,
+    appKey: process.env.CN_LC_APP_KEY
+  });
+  g_rtm = new Realtime({
     appId: process.env.CN_LC_APP_ID,
     appKey: process.env.CN_LC_APP_KEY
   });
@@ -41,9 +51,9 @@ async function checkStorage(res) {
     }
 
     await testObj.destroy();
-    res.send(`✓ ${g_region} Storage (read/write/delete)`);
+    res.send(`✓ ${g_region} LeanStorage (read/write/delete)`);
   } catch (e) {
-    res.send(`✗ ${g_region} Storage (read/write/delete): ${e}`);
+    res.send(`✗ ${g_region} LeanStorage (read/write/delete): ${e}`);
   }
 }
 
@@ -60,13 +70,29 @@ async function checkLeanEngineWeb(res, url) {
   }
 }
 
+async function checkRTM(res) {
+  try {
+    const alice = await g_rtm.createIMClient('alice');
+    const conv = await alice.createConversation({members: ['bob'], name: 'test'});
+    const msg = await conv.send(new TextMessage('test msg'));
+    if (msg.getText() !== 'test msg') {
+      throw new Error('Alice did not receive back expected message.');
+    }
+    res.send(`✓ ${g_region} LeanMessage`);
+  } catch (e) {
+    res.send(`✗ ${g_region} LeanMessage: ${e}`);
+  }
+}
+
 module.exports = robot => {
   robot.respond(/check/i, async res => {
     initSdkForUS();
     await checkStorage(res);
     await checkLeanEngineWeb(res, process.env.US_LC_LE_WEB_URL);
+    await checkRTM(res);
     initSdkForCN();
     await checkStorage(res);
     await checkLeanEngineWeb(res, process.env.CN_LC_LE_WEB_URL);
+    await checkRTM(res);
   });
 };
